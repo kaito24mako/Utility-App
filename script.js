2b35aec8ce9e4de8f57d11e9338689fe4f15d5fd
kaito24mako <kaitowatanabemcc@gmail.com>
2025-09-27 22:26:01 +1000

    create event for light switches

---
+++ script.js
@@ -0,0 +1,15 @@
+// Light switch
+
+const lightButton = document.querySelector("#light");
+const darkButton = document.querySelector("#dark");
+
+lightButton.addEventListener("click", () => {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+})
+
+darkButton.addEventListener("click", () => {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+})
+
