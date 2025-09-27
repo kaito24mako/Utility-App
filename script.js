b4913c666bbab9da24c6c46db37c72912e998d59
kaito24mako <kaitowatanabemcc@gmail.com>
2025-09-28 01:03:54 +1000

    put counter event listeners into one function for conciseness

--- script.js
+++ script.js
@@ -21,21 +21,28 @@ const resetButton = document.querySelector("#resetBtn");
 const numberInput = document.querySelector("#inputBox");
 const numberOutput = document.querySelector("#result");
 
+let action = "";
+
+function counterTool(action) {
+    let currentValue = Number(numberInput.value);
+
+    if (action === "increase") {
+        numberInput.value = currentValue + 1;  
+    } else if (action === "decrease") {
+        numberInput.value = currentValue - 1;
+    } else if (action === "reset") {
+        numberInput.value = 0;
+    }
+
+    numberOutput.textContent = `${numberInput.value}`;
+};
+
 increaseButton.addEventListener("click", () => {
-    let currentValue = Number(numberInput.value);
-    numberInput.value = currentValue + 1;
-    console.log(numberInput.value);
-    numberOutput.textContent = `${numberInput.value}`;
+    counterTool("increase");
 });
-
 decreaseButton.addEventListener("click", () => {
-    let currentValue = Number(numberInput.value);
-    numberInput.value = currentValue - 1;
-    console.log(numberInput.value);
-    numberOutput.textContent = `${numberInput.value}`;
+    counterTool("decrease");
 });
-
 resetButton.addEventListener("click", () => {
-    numberInput.value = 0;
-    numberOutput.textContent = `${numberInput.value}`;
+    counterTool("reset");
 });
